c6f75baf26022cf90b1e0e306771c19a316b08fd
Giovanni D'amico <giovannidamico@Giovannis-MacBook-Pro.local>
2020-06-05 17:38:12 +0200

    added persistance to redux for the cart items

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-import store from './redux/store';
+import { PersistGate } from "redux-persist/integration/react";
+import redux from "./redux/store";
+import { Provider } from "react-redux";
 
-import {BrowserRouter} from "react-router-dom";
-import {Provider} from "react-redux";
-
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider store={redux.store}>
     <BrowserRouter>
       <React.StrictMode>
-        <App />
+        <PersistGate persistor={redux.persistor}>
+          <App />
+        </PersistGate>
       </React.StrictMode>
     </BrowserRouter>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
