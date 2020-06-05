b8dfec6c72a5850544013e4a93e5a5ef0c0781cc
Giovanni D'amico <giovannidamico@Giovannis-MacBook-Pro.local>
2020-06-05 18:00:15 +0200

    moved directory to redux

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,53 +1,22 @@
-import React from 'react';
-import MenuItem from '../menu-item/menu-item.component';
+import React from "react";
+import MenuItem from "../menu-item/menu-item.component";
 
-import './directory.styles.scss';
+import { createStructuredSelector } from "reselect";
 
-class Directory extends React.Component {
-  constructor() {
-    super();
+import "./directory.styles.scss";
+import { selectDirectorySections } from "../../redux/directory/directory.selectors";
+import { connect } from "react-redux";
 
-    this.state = {
-      sections: [
-        {
-            title: 'NEW ARRIVALS',
-            imageUrl: 'https://i.ibb.co/XXcZndd/alberto-bobbera-URw-OHJu-TZas-unsplash.jpg',
-            size: 'large',
-            id: 1,
-            linkUrl : "/shop"
-        },
-        {
-            title: 'SALES',
-            imageUrl: 'https://i.ibb.co/9htX57M/barbora-polednova-Hx3k-Z7-GB4dc-unsplash.jpg',
-            size: 'large',
-            id: 2,
-            linkUrl : "/shop"
-        },
-        {
-            title: 'sneakers',
-            imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-            id: 3,
-            linkUrl : "/shop"
-        },
-        {
-            title: 'sneakers',
-            imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-            id: 4,
-            linkUrl : "/shop"
-        }
-      ]
-    };
-  }
+const Directory = ({ sections }) => (
+  <div className="directory-menu">
+    {sections.map(({ id, ...otherSectionProps }) => (
+      <MenuItem key={id} {...otherSectionProps} />
+    ))}
+  </div>
+);
 
-  render() {
-    return (
-      <div className='directory-menu'>
-        {this.state.sections.map(({ id, ...otherSectionProps }) => (
-          <MenuItem key={id} {...otherSectionProps}/>
-        ))}
-      </div>
-    );
-  }
-}
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections,
+});
 
-export default Directory;
+export default connect(mapStateToProps)(Directory);
---
+++ src/redux/directory/directory.reducer.js
@@ -0,0 +1,41 @@
+const INITIAL_STATE = {
+  sections: [
+    {
+      title: "NEW ARRIVALS",
+      imageUrl:
+        "https://i.ibb.co/XXcZndd/alberto-bobbera-URw-OHJu-TZas-unsplash.jpg",
+      size: "large",
+      id: 1,
+      linkUrl: "/shop",
+    },
+    {
+      title: "SALES",
+      imageUrl:
+        "https://i.ibb.co/9htX57M/barbora-polednova-Hx3k-Z7-GB4dc-unsplash.jpg",
+      size: "large",
+      id: 2,
+      linkUrl: "/shop",
+    },
+    {
+      title: "sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+      id: 3,
+      linkUrl: "/shop",
+    },
+    {
+      title: "sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+      id: 4,
+      linkUrl: "/shop",
+    },
+  ],
+};
+
+const directoryReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default directoryReducer;
