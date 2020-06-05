5bf4c61b8b531218fd99194f69be8ec49206a360
Giovanni D'amico <giovannidamico@Giovannis-MacBook-Pro.local>
2020-06-05 14:20:18 +0200

    added quantity to cart items

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,40 +1,42 @@
-import React from "react"
-import {Link} from "react-router-dom"
-import "./header.styles.scss"
-import {ReactComponent as Logo} from "../../assets/crown.svg"
-import { auth } from "../../firebase/firebase.util"
-import { connect } from "react-redux"
-import CartIcon from "../cart-icon/cart-icon.component"
-import CartDropDown from "../cart-dropdown/cart-dropdown.component"
+import React from "react";
+import { Link } from "react-router-dom";
+import "./header.styles.scss";
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+import { auth } from "../../firebase/firebase.util";
+import { connect } from "react-redux";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropDown from "../cart-dropdown/cart-dropdown.component";
 
-const Header = ({currentUser, hidden}) => (
-    <div className="header">
-        <Link className="logo-container" to="/">
-            <Logo className="logo"></Logo>
-        </Link>
-        <div className="options">
-            <Link className="option" to="/shop">
-                SHOP
-            </Link>
-            <Link className="option" to="/signIn">
-                CONTACT
-            </Link>
-            {currentUser ? 
-            <div className="option" onClick={()=> auth.signOut()}>SIGN OUT</div> :
-            <Link className="option" to="/signin">
-                SIGN IN
-            </Link>
-            }
-            <CartIcon/>
+const Header = ({ currentUser, hidden }) => (
+  <div className="header">
+    <Link className="logo-container" to="/">
+      <Logo className="logo"></Logo>
+    </Link>
+    <div className="options">
+      <Link className="option" to="/shop">
+        SHOP
+      </Link>
+      <Link className="option" to="/signIn">
+        CONTACT
+      </Link>
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          SIGN OUT
         </div>
-        {hidden ? null:
-        <CartDropDown/>}
+      ) : (
+        <Link className="option" to="/signin">
+          SIGN IN
+        </Link>
+      )}
+      <CartIcon />
     </div>
-)
+    {hidden ? null : <CartDropDown />}
+  </div>
+);
 
-const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
-    currentUser,
-    hidden
-})
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
+});
 
 export default connect(mapStateToProps)(Header);
--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -5,95 +5,97 @@ import CustomButton from "../custom-button/custom-button.component";
 
 import { auth, createUserProfileDocument } from "../../firebase/firebase.util";
 
-import "./sign-up.styles.scss"
+import "./sign-up.styles.scss";
 
 class SignUp extends React.Component {
-    constructor(){
-        super();
+  constructor() {
+    super();
 
-        this.state={
-            displayName: '',
-            email: '',
-            password: '',
-            confirmPassword: '',
-        }
+    this.state = {
+      displayName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    };
+  }
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const { displayName, email, password, confirmPassword } = this.state;
+
+    if (password !== confirmPassword) {
+      alert("passwords do not match");
+      return;
     }
 
-    handleSubmit= async (e) =>{
-        e.preventDefault();
-
-        const { displayName, email, password, confirmPassword } = this.state;
-
-        if(password !== confirmPassword){
-            alert("passwords do not match");
-            return;
-        }
-
-        try{
-            const {user} = await auth.createUserWithEmailAndPassword(email,password);
-            await createUserProfileDocument(user, { displayName })
-            this.setState({
-                displayName: '',
-                email: '',
-                password: '',
-                confirmPassword: '',
-            });
-        } catch(error){
-            console.error(error);
-        }
+    try {
+      const { user } = await auth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+      await createUserProfileDocument(user, { displayName });
+      this.setState({
+        displayName: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+      });
+    } catch (error) {
+      console.error(error);
     }
+  };
 
-    handleChange = (e) => {
-        const {value,name} = e.target;
+  handleChange = (e) => {
+    const { value, name } = e.target;
 
-        this.setState({[name]:value})
-    }
+    this.setState({ [name]: value });
+  };
 
-    render(){
-        const {displayName, email, password, confirmPassword} = this.state;
-        return(
-            <div className="sign-up">
-                <h2 className="title">I do not have an account</h2>
-                <span>Sign up with your email and password</span>
-                <form className="sign-up-form" onSubmit={this.handleSubmit}>
-                <FormInput
-                    type="text"
-                    name="displayName"
-                    label="Display Name"
-                    value={displayName}
-                    required
-                    onChange={this.handleChange}
-                />
-                <FormInput
-                    type="text"
-                    name="email"
-                    label="Email"
-                    value={email}
-                    required
-                    onChange={this.handleChange}
-                />
-                 <FormInput
-                    type="password"
-                    name="password"
-                    label="Password"
-                    value={password}
-                    required
-                    onChange={this.handleChange}
-                />
-                 <FormInput
-                    type="password"
-                    name="confirmPassword"
-                    label="Confirm Password"
-                    value={confirmPassword}
-                    required
-                    onChange={this.handleChange}
-                />
-                <CustomButton type="submit">SIGN UP</CustomButton>
-                </form> 
-
-            </div>
-        )
-    }
+  render() {
+    const { displayName, email, password, confirmPassword } = this.state;
+    return (
+      <div className="sign-up">
+        <h2 className="title">I do not have an account</h2>
+        <span>Sign up with your email and password</span>
+        <form className="sign-up-form" onSubmit={this.handleSubmit}>
+          <FormInput
+            type="text"
+            name="displayName"
+            label="Display Name"
+            value={displayName}
+            required
+            onChange={this.handleChange}
+          />
+          <FormInput
+            type="text"
+            name="email"
+            label="Email"
+            value={email}
+            required
+            onChange={this.handleChange}
+          />
+          <FormInput
+            type="password"
+            name="password"
+            label="Password"
+            value={password}
+            required
+            onChange={this.handleChange}
+          />
+          <FormInput
+            type="password"
+            name="confirmPassword"
+            label="Confirm Password"
+            value={confirmPassword}
+            required
+            onChange={this.handleChange}
+          />
+          <CustomButton type="submit">SIGN UP</CustomButton>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SignUp;
